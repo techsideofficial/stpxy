c881ec7ee1e65fcd938be602fe20d55d4f384ed1
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-03 11:40:53 +0000

    refactor: rename to verifyQuery

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.options("*", (req, res, next) => {
 app.all("*", (req, res, next) => {
     async function clojure() {
         lib.verifyKey(req);
-        lib.verifyBrand(req);
+        lib.verifyQuery(req);
         await new Promise((resolve, reject) => {
             try {
                 // constructs the initial options object with the
